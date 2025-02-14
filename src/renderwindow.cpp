53044e37e6f1de92f358b3d8ac6c5d56bfe23892
jarreed0 <jarreed0@gmail.com>
2025-02-13 18:01:11 -0600

    update renderer and window functions

--- src/renderwindow.cpp
+++ src/renderwindow.cpp
@@ -16,12 +16,13 @@ RenderWindow::RenderWindow()
 
 void RenderWindow::create(const char* p_title, int p_w, int p_h)
 {
-	window = SDL_CreateWindow(p_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, p_w, p_h, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow(p_title, p_w, p_h, 0);
+        SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
 
 	if (window == NULL)
 		std::cout << "Window failed to init. Error: " << SDL_GetError() << std::endl;
 
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	renderer = SDL_CreateRenderer(window, NULL);
 }
 
 
